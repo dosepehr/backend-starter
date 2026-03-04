fc06f7e7b57187d073b0ceb9cd2efe04f194576e
dosepehr <dosepehr@gmail.com>
2026-03-05 00:55:20 +0330

    add env port config to main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // global port
+  const PORT = process.env.PORT ?? 3000;
+  console.log(`app is running on port ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
