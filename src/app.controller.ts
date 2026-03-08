62253ffa169f758c9856ab27c40ce3cff0c64de4
dosepehr <dosepehr@gmail.com>
2026-03-09 01:46:28 +0330

    add admin only route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ResponseMessage } from 'utils/decorators/response-message.decorator';
+import { AuthGuard } from 'utils/guards/auth.guard';
+import { RolesGuard } from 'utils/guards/roles.guard';
+import { Roles } from 'utils/decorators/roles.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { CurrentUser } from 'utils/decorators/current-user.decorator';
+import { type AuthenticatedUser } from 'utils/interfaces/jwt-payload.interface';
+import { UserRole } from './modules/users/enums/user-role.enum';
 
 @Controller()
 export class AppController {
@@ -10,5 +17,13 @@ export class AppController {
   @ResponseMessage('Welcome to Nest Starter')
   getHello() {
     return this.appService.getHello();
+  }
+  @Get('admin-only')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth('access-token')
+  @ResponseMessage('Welcome to Admin')
+  adminRoute(@CurrentUser() user: AuthenticatedUser) {
+    return { user: user };
   }
 }
